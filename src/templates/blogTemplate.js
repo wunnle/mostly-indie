7f1c93f7a081966d8afe2b5baa77a7e6ea1ce896
Sinan <me@wunnle.com>
2019-12-24 17:38:03 +0300

    updated post style

--- src/templates/blogTemplate.js
+++ src/templates/blogTemplate.js
@@ -1,21 +1,25 @@
 import { graphql } from 'gatsby'
 import React from 'react'
 
-export default function Template({
+import styles from './blogTemplate.module.css'
+
+const Template = ({
   data // this prop will be injected by the GraphQL query below.
-}) {
+}) => {
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
     <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
-        <div className="blog-post-content" dangerouslySetInnerHTML={{ __html: html }} />
+      <div className={styles.post}>
+        <h1 className={styles.title}>{frontmatter.title}</h1>
+        <p className={styles.date}>{frontmatter.date}</p>
+        <div className={styles.content} dangerouslySetInnerHTML={{ __html: html }} />
       </div>
     </div>
   )
 }
+
+export default Template
 
 export const pageQuery = graphql`
   query($path: String!) {
