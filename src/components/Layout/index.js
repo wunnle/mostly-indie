42a0a20933e226739acbca18d5578b9055b86d13
Sinan <me@wunnle.com>
2020-01-12 23:56:34 +0300

    added favicon

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,12 +2,17 @@ import './layout.css'
 
 import PropTypes from 'prop-types'
 import React from 'react'
+import Helmet from 'react-helmet'
 
+import favicon from '../../images/favicon.ico'
 import Header from '../Header'
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Header />
       <main>{children}</main>
     </>
