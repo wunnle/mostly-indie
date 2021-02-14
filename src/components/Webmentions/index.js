aa0a71725c5499b348ab2e77f18ba8567254f2e2
Sinan <me@wunnle.com>
2021-02-14 11:08:50 +0300

    not showing likes in webmentions

--- src/components/Webmentions/index.js
+++ src/components/Webmentions/index.js
@@ -5,11 +5,10 @@ import styles from './Webmentions.module.css'
 const wpType = {
   'like-of': 'liked',
   'in-reply-to': 'replied',
-  'repost-of': 'posted'
+  'repost-of': 'retweeted'
 }
 
 const Webmentions = ({ mentions }) => {
-  const likesCount = mentions.filter(m => m.wmProperty === 'like-of').length
   const replyCount = mentions.filter(m => m.wmProperty === 'in-reply-to').length
   const postCount = mentions.filter(m => m.wmProperty === 'repost-of').length
 
@@ -23,50 +22,51 @@ const Webmentions = ({ mentions }) => {
         <h3 className={styles.title}>Mentions</h3>
         <p className={styles.counts}>
           {[
-            postCount > 0 && `${postCount} posts`,
-            replyCount > 0 && `${replyCount} replies`,
-            likesCount > 0 && `${likesCount} likes`
+            postCount > 0 && `${postCount} retweets`,
+            replyCount > 0 && `${replyCount} replies`
           ]
             .filter(a => a)
             .join(', ')}
         </p>
       </hgroup>
-      {mentions.filter(m => m.wpProperty !== 'like-of').map(m => (
-        <div key={m.wmId} className={styles.mention}>
-          <div className={styles.profilePhotoHolder}>
-            <img
-              src={m.author.photo}
-              alt={m.author.name}
-              className={styles.profilePhoto}
-            />
-          </div>
-          <div className={styles.content}>
-            <a
-              href={m.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.link}
-            >
-              <strong className={styles.author}>{m.author.name}</strong>{' '}
-              <span className={styles.mentionAction}> {wpType[m.wmProperty]}</span>
-            </a>
-            <div className={styles.mentionText}>
-              {m.content?.text ? (
-                m.content.text
-              ) : (
-                <a
-                  href={m.url}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className={styles.url}
-                >
-                  {m.url.replace(/https?:\/\//, '')}
-                </a>
-              )}
+      {mentions
+        .filter(m => m.wmProperty !== 'like-of')
+        .map(m => (
+          <div key={m.wmId} className={styles.mention}>
+            <div className={styles.profilePhotoHolder}>
+              <img
+                src={m.author.photo}
+                alt={m.author.name}
+                className={styles.profilePhoto}
+              />
+            </div>
+            <div className={styles.content}>
+              <a
+                href={m.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className={styles.link}
+              >
+                <strong className={styles.author}>{m.author.name}</strong>{' '}
+                <span className={styles.mentionAction}> {wpType[m.wmProperty]}</span>
+              </a>
+              <div className={styles.mentionText}>
+                {m.content?.text ? (
+                  m.content.text
+                ) : (
+                  <a
+                    href={m.url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.url}
+                  >
+                    {m.url.replace(/https?:\/\//, '')}
+                  </a>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
     </div>
   )
 }
